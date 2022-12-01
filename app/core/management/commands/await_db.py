16be7eb8b40ed03b033d7206a52ba3b4f82b71c0
podmar <marta.podziewska@gmail.com>
2022-12-01 19:42:58 +0100

    fixed typos in stdout

--- app/core/management/commands/await_db.py
+++ app/core/management/commands/await_db.py
@@ -21,7 +21,7 @@ class Command(BaseCommand):
                 self.check(databases=["default"])
                 db_up = True
             except (Psycopg2OperationalError, OperationalError):
-                self.stdout.write("Database unvawailable, waiting 1 second.")
+                self.stdout.write("Database unavailable, waiting 1 second.")
                 time.sleep(1)
 
         self.stdout.write(self.style.SUCCESS("Connected to Database"))
